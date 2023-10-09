3c5d2ffcc56756987c2b2aaa1d0db6397d6a7931
syw(laptop) <syw9846@naver.com>
2023-10-09 12:48:55 +0900

    다시해보기

--- boy.py
+++ boy.py
@@ -1,26 +1,44 @@
 from pico2d import load_image
 
-class IDLE:
-    @staticmethod
-    def do():
-        pass
 
+# 객체 생성 용도가 아닌 함수 그립핑 용도다 self는 필요없다.
+class Idle:
     @staticmethod
     def enter():
+        print('IDLE Entered')
+        pass
+
+    @staticmethod
+    def exit():
+        print('IDLE Exit')
+        pass
+
+    @staticmethod
+    def do():
+        print('IDLE Do')
+        pass
+
+    @staticmethod
+    def draw():
         pass
 
 
+class StateMachine:
+    def __int__(self):
+        self.cur_state = Idle  # Idle()가 아니다 Idle라는 그룹이다.
+        pass
 
+    def start(self):
+        self.cur_state.enter()
+        pass
 
+    def update(self):
+        self.cur_state.do()
+        pass
 
-
-
-
-
-
-
-
-
+    def draw(self):
+        self.cur_state.draw()
+        pass
 
 
 class Boy:
@@ -30,12 +48,18 @@ class Boy:
         self.dir = 0
         self.action = 3
         self.image = load_image('animation_sheet.png')
+        self.state_machine = StateMachine()
+        self.state_machine.start()
 
     def update(self):
         self.frame = (self.frame + 1) % 8
+        self.state_machine.update()
+
 
     def handle_event(self, event):
         pass
 
     def draw(self):
         self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
+        self.state_machine.draw()
+
